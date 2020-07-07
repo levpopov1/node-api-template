e0d85395823052de75c2defec172c6d7bc8c1bf6
Lev Popov <levpopov1@gmail.com>
2020-07-07 18:27:36 +0100

    adds basic db search middleware

---
+++ src/middleware/findAll.js
@@ -0,0 +1,10 @@
+module.exports = function(Model){
+    return async function(req, res, next){
+        try {
+            req.items = await Model.find({});
+            next();
+        } catch (error) {
+            next(error);
+        }
+    };
+};
