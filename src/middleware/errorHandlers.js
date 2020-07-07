2ba392224ce397d652e693ee8a5bdf1cabe7b539
Lev Popov <levpopov1@gmail.com>
2020-07-07 18:27:53 +0100

    adds basic error handling middleware

---
+++ src/middleware/errorHandlers.js
@@ -0,0 +1,19 @@
+function notFound(req, res, next) {
+    res.status(404).json({
+        statusCode: 404,
+        message: "Not Found"
+    });
+}
+
+function genericErrorHandler(err, req, res, next) {
+    const statusCode = res.statusCode !== 200 ? res.statusCode : 500;
+    res.status(statusCode).json({
+        message: err.message,
+        stack: process.env.NODE_ENV === 'production' ? null : err.stack
+    });
+}
+
+module.exports = {
+    notFound,
+    genericErrorHandler
+};
