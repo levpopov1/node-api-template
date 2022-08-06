cc52652c5257312eb0f7a484307283477f9cf8a7
Lev Popov <levpopov1@gmail.com>
2022-08-06 19:50:26 +0100

    update deps

--- src/server.js
+++ src/server.js
@@ -1,16 +1,16 @@
 // main entry point for API server
-const express = require('express');
-const passport = require('passport');
-const cors = require('cors');
-const morgan = require('morgan');
-const routes = require('./routes');
-const connectDB  = require('./db')
+const express = require("express");
+const passport = require("passport");
+const cors = require("cors");
+const morgan = require("morgan");
+const routes = require("./routes");
+const connectDB = require("./db");
 
 // sets environment variables based on centents of .env file
-require('dotenv').config();
+require("dotenv").config();
 
 // db
-connectDB();
+// connectDB();
 
 // start server instance
 const app = express();
@@ -25,19 +25,20 @@ app.set('trust proxy', true);
 // cross origin request middleware
 app.use(cors());
 // dev middleware
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 // url middleware
 app.use(express.json());
 // session management middleware
-app.use(passport.initialize());
-app.use(passport.session());
-
+// app.use(passport.initialize());
+// app.use(passport.session());
 
 // route handlers entrypoint for all routes
-app.use('/api/v1', routes);
+app.use("/api/v1", routes);
 
 // begin listening on given port
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, function(){
-    console.log(`The app is running in ${process.env.NODE_ENV} mode on port: ${PORT}`);
+app.listen(PORT, function () {
+  console.log(
+    `The app is running in ${process.env.NODE_ENV} mode on port: ${PORT}`
+  );
 });
