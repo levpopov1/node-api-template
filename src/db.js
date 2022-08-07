309911669c1ad68186fa58078bb10eb5750fd9bb
Lev Popov <levpopov1@gmail.com>
2022-08-07 16:41:37 +0100

    update mongodb connection to mongoose v6

--- src/db.js
+++ src/db.js
@@ -1,18 +1,13 @@
 // database connetion setup
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const connectDB = async function(){
-    try {
-        const conn = await mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser: true, 
-            useFindAndModify : false, 
-            useUnifiedTopology: true
-        });
-        console.log("MongoDB connetcion successful");
-
-    } catch (error) {
-        console.error(error);
-    }
-}
+const connectDB = async function () {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+    console.log("MongoDB connetcion successful");
+  } catch (error) {
+    console.error(error);
+  }
+};
 
 module.exports = connectDB;
