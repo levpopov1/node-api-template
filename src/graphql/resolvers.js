64aaaa3a022b69a4acd73d3fa220040bbf0608f5
Lev Popov <levpopov1@gmail.com>
2022-08-30 15:48:26 +0000

    Ts migrate

--- src/graphql/resolvers.js
+++
@@ -1,8 +0,0 @@
-import { Person } from "../model/Person.js";
-
-export const Resolvers = {
-  Query: {
-    people: async () => await Person.find(),
-    person: async (parent, args) => await Person.findById(args.id),
-  },
-};
